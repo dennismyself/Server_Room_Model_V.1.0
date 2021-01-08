538cacdf7f908b46a3ca632f2251720ad1e1cd6f
dennismyself <dennis.qianjunyi@gmail.com>
2021-01-08 16:26:40 +1100

    Initial Commitment

---
+++ Setup.m
@@ -0,0 +1,7 @@
+% ssc_refrigeration
+ssc_building_ventilation
+InitialTemp = 300; %k
+% ground_temperature = 283;
+zone_volume = building_length * building_width * building_height/ 8;
+zone_flow_area = duct_area;
+% environment_temperature = 300;
